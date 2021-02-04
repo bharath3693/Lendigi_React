670ce1114760a38e1a6efb75d164397aba1821b2
bharath3693 <saibharath3693@gmail.com>
2021-02-04 13:23:38 +0530

    FIRST GIT

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import "./Styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>APP . JSX</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
